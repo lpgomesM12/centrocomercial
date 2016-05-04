7e8a74331d32e7b008c3093f79edc551ead27e7e
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-05-04 14:48:02 -0300

    adicionando exeção metodo pesquisa produtos login

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,7 @@
 class ProdutosController < ApplicationController
-  before_action :set_produto, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  
+ before_action :authenticate_user!, :except => [:buscaprodutos,:BuscaCategorias] 
+ before_action :set_produto, only: [:show, :edit, :update, :destroy]
 
  # Pesquisa por produto
 
