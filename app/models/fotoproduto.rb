9381fd0eca372be3c19c00d1b7fbb091ed375656
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-05-20 14:07:03 -0300

    alterando redimensionamento de foto

--- app/models/fotoproduto.rb
+++ app/models/fotoproduto.rb
@@ -1,7 +1,7 @@
 class Fotoproduto < ActiveRecord::Base
   belongs_to :produto
 
-  has_attached_file :imagem, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :imagem, styles: { medium: "800x800>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :imagem, content_type: /\Aimage\/.*\Z/
 
 end
