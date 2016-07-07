38f6977d4eccd7415c3cf7f3b31456fe20dea445
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-07-07 19:04:28 -0300

    alteração layout e empresa

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,16 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!
   def index
+   @empresa = Empresa.new
+
+   if params[:id]  != "" 
+   
+     @User = User.find(current_user.id)
+     
+     @User.empresa_id = params[:id]
+     @User.save
+
+   end
 
   end
 end
