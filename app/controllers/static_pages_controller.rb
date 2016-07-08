cb74079c4efcccebbe624c3544b3e5563ebc4144
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2016-07-08 08:20:29 -0300

    alteração pagina

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,11 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate_user!
   def index
-   @empresa = Empresa.new
-
-   if params[:id]  != "" 
+   
+   if params[:id] != "" && params[:id] != nil 
    
      @User = User.find(current_user.id)
-     
+  
      @User.empresa_id = params[:id]
      @User.save
 
